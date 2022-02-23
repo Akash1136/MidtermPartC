039b74b0af46c004ab05afd0fb7334f8793787ee
Akash1136 <pate3576@sheridancollege.ca>
2022-02-23 10:59:32 -0500

    Genre information added

---
+++ src/TestUserProfile.java
@@ -0,0 +1,22 @@
+
+import java.util.Scanner;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Akash
+ */
+public class TestUserProfile {
+   public static void main(String[] args) 
+   {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter your name:");
+    String name = sc.nextLine();
+    System.out.println("Select any one genres number:");
+    int a = sc.nextInt();
+   }
+}
